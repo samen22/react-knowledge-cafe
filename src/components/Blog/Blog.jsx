545c3d19bee6799bb3f032b9d6c27f8de328bbe2
samen22 <samenjoy27@gmail.com>
2023-09-25 18:08:34 +0600

    added blogs contents from the fake API

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
-
+import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
-    console.log(blog);
+    const {title, cover, author, author_img, reading_time, posted_date, hashtags} = blog;
     return (
         <div>
-            
+            <img src={cover} alt={`cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+
+                </div>
+            </div>
+            <h2 className='text-4xl'>{title}</h2>
+            <p className='space-x-3'>
+                {
+                    hashtags.map((hash, idx)=> <span key={idx}><a href="">{hash}</a></span> )
+                }
+            </p>
+
         </div>
     );
 };
 
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired
+}
 export default Blog;
